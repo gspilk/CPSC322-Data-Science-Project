58a5dd29ad2b3ea2b290188ee05f16eb44f820af
gspilk <gspilker@zagmail.gonzaga.edu>
2024-12-09 23:09:13 +0000

    started making flask app

--- flaskapp/predict.py
+++ flaskapp/predict.py
@@ -1,1 +1,14 @@
 #to do: create the flask which allows users to predict using the random forests algorithm.
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Welcome to the March Madness Predictor App<h1>",200
+
+
+
+if __name__ == "__main__":
+   app.run(host="0.0.0.0",port=5001, debug=True) 
