8d4ab04c3a594f58b8808d5e8a5014dfca0106f4
gspilk <123699226+gspilk@users.noreply.github.com>
2024-12-10 16:06:26 -0800

    Update marchmadnesspredict.py

--- flaskapp/marchmadnesspredict.py
+++ flaskapp/marchmadnesspredict.py
@@ -1,18 +1,26 @@
 import requests # a lib for making http requests
 import json # a lib for working with json
 
-url = "http://localhost:5001/predict?TEAM=Duke&ADJOE=125.2&ADJDE=90.6&EFF_O=0.9764&EFF_D=56.6&YEAR=2015"
+url = "https://march-madness-champion-predictor.onrender.com"
 
-response = requests.get(url=url)
+# Make a GET request to the /predict endpoint with query parameters
+response = requests.get(url, params={
+    "TEAM": "Duke", 
+    "ADJOE": "125.2",
+    "ADJDE": "90.6",
+    "EFF_O": "0.9764",
+    "EFF_D": "56.6",
+    "YEAR": "2015"
+})
 
-# first thing, check the response's status_code
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
-# https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#successful_responses
+# Check if the request was successful (status code 200)
 print(response.status_code)
 if response.status_code == 200:
     # STATUS OK
-    # we can extract the prediction from the response's JSON text
+    # Extract the prediction from the response's JSON text
     json_object = json.loads(response.text)
     print(json_object)
     pred = json_object["prediction"]
-    print("prediction:", pred)
+    print("Prediction:", pred)
+else:
+    print("Error:", response.status_code)
