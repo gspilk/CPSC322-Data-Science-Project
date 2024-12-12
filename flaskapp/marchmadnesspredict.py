ad23d11b44186d7c1b9c98f4b41d020b7d9eeb02
gspilk <123699226+gspilk@users.noreply.github.com>
2024-12-11 21:56:23 -0800

    Updated marchmadnesspredict.py to be able to enter data and click a button to make the prediction on how far they made it into the tournament in previous years.

--- flaskapp/marchmadnesspredict.py
+++ flaskapp/marchmadnesspredict.py
@@ -1,77 +1,73 @@
-#to do: create the flask which allows users to predict using the random forests algorithm.
 import pickle
 from flask import Flask, request, jsonify
-import numpy as np
 
 app = Flask(__name__)
 
-def load_model():
-    # unpickle header and tree in tree.p
-    infile = open("flaskapp/tree.p", "rb")
-    header, tree = pickle.load(infile)
-    infile.close()
+# Load the pickled tree
+def load_tree():
+    with open("flaskapp/tree.p", "rb") as infile:
+        header, tree = pickle.load(infile)
     return header, tree
-def predict(self, X_test):
-        """Makes predictions for test instances in X_test."""
-        distances, neighbor_indices = self.kneighbors(X_test)
-        y_predicted = []
-        
-        
+
+# Prediction logic using a decision tree
+def predict_tree(tree, instance, header):
+    if tree[0] == "Leaf":
+        return tree[1]  # Return the prediction from the leaf
+    elif tree[0] == "Attribute":
+        attribute = tree[1]
+        attr_index = header.index(attribute)
+        instance_value = instance[attr_index]
+
+        for branch in tree[2:]:
+            if branch[0] == "Value" and branch[1] == instance_value:
+                return predict_tree(branch[2], instance, header)
+    return None  # If no matching branch is found
+
 @app.route("/")
 def index():
     return """
+    <h1>Welcome to the March Madness Predictor App</h1>
+    <img src="https://i.bleacherreport.net/images/team_logos/328x328/gonzaga_basketball.png?canvas=492,328" alt="Gonzaga Bulldogs">
 
-        <h1>Welcome to the March Madness Predictor App</h1>
-        <img src="https://i.bleacherreport.net/images/team_logos/328x328/gonzaga_basketball.png?canvas=492,328" alt="Gonzaga Bulldogs">
+    <form action="/predict" method="get">
+        <label for="TEAM">TEAM:</label>
+        <input type="text" id="TEAM" name="TEAM"><br><br>
         
-        <!-- Example button to trigger a prediction or form submission -->
-        <form action="https://march-madness-tournament-predictor.onrender.com/" method="get">
-            <label for="TEAM">Team:</label>
-            <input type="text" id="TEAM" name="TEAM"><br><br>
-            
-            <label for="ADJOE">ADJOE:</label>
-            <input type="text" id="ADJOE" name="ADJOE"><br><br>
+        <label for="ADJOE">ADJOE:</label>
+        <input type="text" id="ADJOE" name="ADJOE"><br><br>
 
-            <label for="ADJDE">ADJDE:</label>
-            <input type="text" id="ADJDE" name="ADJDE"><br><br>
+        <label for="ADJDE">ADJDE:</label>
+        <input type="text" id="ADJDE" name="ADJDE"><br><br>
 
-            <label for="EFF_O">EFF_O:</label>
-            <input type="text" id="EFF_O" name="EFF_O"><br><br>
+        <label for="YEAR">YEAR:</label>
+        <input type="text" id="YEAR" name="YEAR"><br><br>
 
-            <label for="EFF_D">EFF_D:</label>
-            <input type="text" id="EFF_D" name="EFF_D"><br><br>
-
-            <label for="YEAR">YEAR:</label>
-            <input type="text" id="YEAR" name="YEAR"><br><br>
-
-            <!-- Button to submit the form -->
-            <button type="submit">Make Prediction</button>
-        </form>
+        <button type="submit">Make Prediction</button>
+    </form>
     """, 200
 
-
-# # lets add a route for the /predict endpoint
 @app.route("/predict")
 def predict():
-    # lets parse the unseen instance values from the query string
-    # they are in the request object
-    team = request.args.get("TEAM") # defaults to None
-    adjoe = request.args.get("ADJOE")
-    adjde = request.args.get("ADJDE")
-    eff_o = request.args.get("EFF_O")
-    eff_d = request.args.get("EFF_D")
-    year = request.args.get("YEAR")
+    # Parse query parameters
+    try:
+        team = request.args.get("TEAM")
+        adjoe = float(request.args.get("ADJOE"))
+        adjde = float(request.args.get("ADJDE"))
+        year = int(request.args.get("YEAR"))
+    except (TypeError, ValueError):
+        return "Invalid input parameters. Please provide valid values.", 400
 
-    instance = [team,adjoe,adjde,eff_o,eff_d,year]
-    header, tree = load_model()
-    # lets make a prediction!
-    pred = predict(header, tree, instance)
-    if pred is not None:
-        return jsonify({"prediction": pred}), 200
-    # something went wrong!!
-    return "Error making a prediction", 400
+    # Create instance for prediction
+    instance = [team, adjoe, adjde, year, None]  # `None` is placeholder for POSTSEASON
 
+    # Load the tree and predict
+    header, tree = load_tree()
+    prediction = predict_tree(tree, instance, header)
+
+    return jsonify({
+        "prediction": prediction
+    }), 200
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0",port=5001, debug=False) 
+    app.run(host="0.0.0.0", port=5001, debug=False)
 
